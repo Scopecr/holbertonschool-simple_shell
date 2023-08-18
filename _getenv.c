62c98688cf300bee082849a499da9d6112961fa7
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-18 10:18:10 -0400

    created errors file and get envirement.c added functions to shell.h

---
+++ _getenv.c
@@ -0,0 +1,73 @@
+#include "shell.h"
+
+char *_getenv(const char *name)
+{
+	char **environ_copy;
+	char *variable, *value, *path;
+	int compare;
+	unsigned int path_length, environ_length, length, i;
+
+	environ_length = 0;
+	while (environ[environ_length] != NULL)
+		environ_length++;
+		environ_copy = NULL;
+		environ_copy = copy_env(environ_copy, environ_length, environ_length);
+
+		length = strlen((char *)name);
+		i =0;
+	while (environ_copy[i] != NULL)
+	{
+		variable = environ_copy[i];
+		compare = _strncmp((char *)name, variable, length);
+		if (compare == 1)
+		{
+		if (value == '\0')
+		{
+			errors(4);
+			exit(EXIT_FAILURE);
+		}
+		path_length = _strlen(value);
+		path = malloc(sizeof(char) * path_length + 1);
+		if (path == NULL)
+		{
+			error(3);
+			return (NULL);
+		}
+		path = _strcpy(path, value);
+		free_dp(environ_copy, environ_length);
+		return (path);
+		}
+	i++;
+	}
+return (NULL);
+}
+
+char **copy_env(char **environ_copy, unsigned int environ_length)
+{
+	char *variable;
+	unsigned int variable_length;
+	unsigned int i;
+	
+	environ_copy = malloc(sizeof(char **)* (environ_length));
+	if (environ_copy == NULL)
+	{
+		errors(3);
+		return (NULL);
+	}
+	i = 0;
+	while (i < environ_length)
+	{
+		variable = environ[i];
+		variable_length = _strlen(variable);
+
+		environ_copy[i] = malloc(sizeof(char) * variable_length + 1);
+		if (environ_copy[i] == NULL)
+		{
+			errors(3);
+			return (NULL);
+		}
+		_strcpy(environ_copy[i], environ[i]);
+		i++;
+	}
+	return (environ_copy);
+}
