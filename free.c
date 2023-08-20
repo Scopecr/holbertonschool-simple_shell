bb98971ff72a9e55da2a8d7f90aeca43bd17e294
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-20 15:10:54 -0400

    created free.c to free all the memory udes by tokens path and strings

---
+++ free.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+void free_all(char **tokens, char *path, char *line, char *fullpath, int flag)
+{
+	free(path);
+	free(tokens);
+	free(line);
+	if ( flag == 1)
+		free(fullpath);
+}
+
+
+void free_dp(char **array, unsigned int length)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < length)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
