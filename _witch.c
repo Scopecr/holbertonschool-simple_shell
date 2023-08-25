6bf8332310d68b03cdeabba9dd0ae2bc3b7d9208
daggzu <112585566+daggzu@users.noreply.github.com>
2023-08-25 14:29:54 -0400

    Update _witch.c

--- _witch.c
+++ _witch.c
@@ -3,14 +3,15 @@
 /**
  * _which - searches for a command in the PATH environment variable
  * @command: the command to search for
+ * @fullpath: pointer to store the full path of the command (output)
  * @path: the PATH environment variable
- * Return: pointer to the full path of the command, or NULL if not found
+ * Return: 1 if found, 0 if not found, -1 on error
  */
-char *_which(char *command, char *path)
+int _which(char *command, char **fullpath, char *path)
 {
     unsigned int command_length, path_length;
     char path_copy[PATH_MAX];
-    char *token, *fullpath;
+    char *token;
 
     command_length = strlen(command);
     strncpy(path_copy, path, sizeof(path_copy) - 1);
@@ -20,24 +21,23 @@ char *_which(char *command, char *path)
     while (token != NULL)
     {
         path_length = strlen(token);
-        fullpath = (char *)malloc(path_length + command_length + 2);
-        if (fullpath == NULL)
+        *fullpath = (char *)malloc(path_length + command_length + 2);
+        if (*fullpath == NULL)
         {
             errors(3);
-            return NULL;
+            return -1;
         }
-        strncpy(fullpath, token, path_length);
-        fullpath[path_length] = '/';
-        strncpy(fullpath + path_length + 1, command, command_length);
-        fullpath[path_length + command_length + 1] = '\0';
-        if (access(fullpath, X_OK) == 0)
+        strncpy(*fullpath, token, path_length);
+        (*fullpath)[path_length] = '/';
+        strncpy(*fullpath + path_length + 1, command, command_length);
+        (*fullpath)[path_length + command_length + 1] = '\0';
+        if (access(*fullpath, X_OK) == 0)
         {
-            return fullpath; /* Return the valid fullpath */
+            return 1; /* Found */
         }
-        free(fullpath);
+        free(*fullpath);
         token = strtok(NULL, ":");
     }
 
-    return NULL; /* Return NULL if command not found */
+    return 0; /* Not found */
 }
-
