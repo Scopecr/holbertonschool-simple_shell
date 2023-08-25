b0f75122e4922866169ed8ae33fc8663be6795c2
daggzu <112585566+daggzu@users.noreply.github.com>
2023-08-25 13:01:21 -0400

    Update _getline.c

--- _getline.c
+++ _getline.c
@@ -1,60 +1,36 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h> // Include this header for ssize_t and size_t
 
 /**
  * _getline - take character input and stores it in a pointer
  * @bp: file pointer to read from
  * 
  * Return: line to be stored on getline
-*/
+ */
 char *_getline(FILE *bp)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
-	if (bp == NULL)
-	{
-		fprintf(stderr, "Invalid file pointer\n");
-		exit(EXIT_FAILURE);
-	}
+    if (bp == NULL)
+    {
+        fprintf(stderr, "Invalid file pointer\n");
+        exit(EXIT_FAILURE);
+    }
 
-	read = getline(&line, &len, bp);
-	if (read == -1)
-	{
-		free(line);
-		exit(EXIT_SUCCESS);
-	}
-	else if (read == 0)
-	{
-		free(line);
-		return NULL;
-	}
+    read = getline(&line, &len, bp);
+    if (read == -1)
+    {
+        free(line);
+        exit(EXIT_SUCCESS);
+    }
+    else if (read == 0)
+    {
+        free(line);
+        return NULL;
+    }
 
-	return line;
+    return line;
 }
-/**
- * _getline - take character input ans stores it in a pointer
- * @line: pointer that captures the string of characters
- * @read: reads the character input
- * @len: measure the length of line
- * 
- * Return: line to be stored on getline
-*/
-char *_getline(FILE *bp)
-{
-	char *line;
-	ssize_t read;
-	size_t len;
 
-	line = NULL;
-	len = 0;
-	read = getline(&line, &len, bp);
-	if (read == -1)
-	{
-		free(line);
-		exit(EXIT_SUCCESS);
-	}
-	return (line);
-}
