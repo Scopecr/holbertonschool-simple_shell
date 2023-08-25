dcb846528d8d699afc83cd54a80c99d3a9ca3cf9
daggzu <112585566+daggzu@users.noreply.github.com>
2023-08-25 13:35:28 -0400

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -1,28 +1,30 @@
 #include "shell.h"
+#include <string.h>
 
 char **tokenizer(char *str)
 {
-	char **tokens;
-	char *token;
-	unsigned int i;
+    char **tokens;
+    char *token;
+    unsigned int i;
 
-	tokens = malloc(sizeof(char) * BUFFER);
-	if (tokens == NULL)
-	{
-		errors(3);
-		exit(EXIT_FAILURE);
-	}
+    tokens = malloc(sizeof(char *) * BUFFER);
+    if (tokens == NULL)
+    {
+        errors(3);
+        exit(EXIT_FAILURE);
+    }
 
-	token = strtok(str, "\n\t\r ");
+    token = strtok(str, "\n\t\r ");
 
-	i = 0;
-	while (token != NULL)
-	{
-		tokens[i] = token;
-		token = strtok (NULL, "\n\t\r ");
-		i++;
-	}
-	tokens[i] = NULL;
+    i = 0;
+    while (token != NULL)
+    {
+        tokens[i] = token;
+        token = strtok(NULL, "\n\t\r ");
+        i++;
+    }
+    tokens[i] = NULL;
 
-	return (tokens);
+    return tokens;
 }
+
