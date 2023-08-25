de3c2e6ab59adcc122c360f8c5a1b450fc664e3f
daggzu <diegog8603@gmail.com>
2023-08-24 22:26:13 -0400

    test

--- _witch.c
+++ _witch.c
@@ -1,5 +1,40 @@
 #include "shell.h"
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
+ * _getline - take character input and stores it in a pointer
+ * @bp: file pointer to read from
+ * 
+ * Return: line to be stored on getline
+*/
+char *_getline(FILE *bp)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	if (bp == NULL)
+	{
+		fprintf(stderr, "Invalid file pointer\n");
+		exit(EXIT_FAILURE);
+	}
+
+	read = getline(&line, &len, bp);
+	if (read == -1)
+	{
+		free(line);
+		exit(EXIT_SUCCESS);
+	}
+	else if (read == 0)
+	{
+		free(line);
+		return NULL;
+	}
+
+	return line;
+}
  * _which - searches directories in PATH variable for command
  * @command: to search for
  * @fullpath: full path of command to execute
